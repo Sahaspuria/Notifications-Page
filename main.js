52a68d63acd3e96a4934ca5b688319b3d41d2acc
Sahaspuria <official.faizan.alam@gmail.com>
2022-09-20 00:41:05 +0530

    Script added

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-const btnMark = document.querySelector(".notifications__header--read");
-const count = document.querySelector(".count");
-const userfield = document.querySelectorAll(".userfield");
 
-btnMark.addEventListener("click", function () {
-  count.textContent = 0;
-  userfield.forEach((item) => item.classList.remove("active"));
-});
